850dffc000914d79966b22cbc551c12dee1b8eb7
peter-cusimano <cusimano390@gmail.com>
2023-09-13 08:18:57 -0500

    updates

---
+++ script.js
@@ -0,0 +1,9 @@
+const board = [
+    ["","",""],
+    ["","",""],
+    ["","",""]
+]
+const test =document.querySelector('.cell')
+test[0].addEventListener('click', function(){
+    test[0].textContent = "X"
+})
